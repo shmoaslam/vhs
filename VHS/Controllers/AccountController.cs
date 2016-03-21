24897b3dee1a090b69fcf3cd2a86a0f301868bb3
DharmendraYadavKumar <dharam3579@gmail.com>
2016-03-21 15:41:03 +0530

    Again Check

--- VHS/Controllers/AccountController.cs
+++ VHS/Controllers/AccountController.cs
@@ -18,7 +18,7 @@ using VHS.Interface;
 
 namespace VHS.Controllers
 {
-    //DharmendraYadav Direct Access
+    //Mrara
     public class AccountController : Controller
     {
         private IAccount _lgoin;
