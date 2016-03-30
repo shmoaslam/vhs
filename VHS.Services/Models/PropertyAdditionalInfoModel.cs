a137899f3eb02d9c0b06c5d7932014cbebb41766
DharmendraYadavKumar <dharam3579@gmail.com>
2016-03-30 21:44:09 +0530

    commit

--- VHS.Services/Models/PropertyAdditionalInfoModel.cs
+++ VHS.Services/Models/PropertyAdditionalInfoModel.cs
@@ -46,7 +46,7 @@ namespace VHS.Services.Models
         public int WheelChairId { get; set; }
         public SelectList WheelChairAllowed { get; set; }
 
-        public IEnumerable<BlakOutDate> BlackOutDatsList { get; set; }
+        public List<BlakOutDate> BlackOutDatsList { get; set; }
 
         [Required(ErrorMessage = "Please enter latitude)")]
         public string Latitude { get; set; }
@@ -87,8 +87,8 @@ namespace VHS.Services.Models
 
     public class BlakOutDate
     {
-        public DateTime StartDate { get; set; }
-        public DateTime EndDate { get; set; }
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
     }
     public class PropertyAmenities
     {
